10e7c6f0a3953e449bbc6bbe996a45fbfd116745
bakpark <toloview@naver.com>
2022-02-25 02:11:52 +0900

    fix: add temperature field to userDto

--- src/main/kotlin/com/kdh/tmp/dto/user/UserResponse.kt
+++ src/main/kotlin/com/kdh/tmp/dto/user/UserResponse.kt
@@ -2,10 +2,10 @@ package com.kdh.tmp.dto.user
 
 import com.kdh.tmp.domain.user.User
 
-data class UserResponse(val userId: Long, val phoneNumber: String){
-    companion object{
+data class UserResponse(val userId: Long, val phoneNumber: String, val mannerTemperature: String) {
+    companion object {
         fun from(user: User): UserResponse {
-            return UserResponse(user.userId!!, user.phoneNumber!!)
+            return UserResponse(user.userId!!, user.phoneNumber!!, user.mannerTemperature!!.toString())
         }
     }
 }
